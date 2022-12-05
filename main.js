f0405e3816c5181efc7c355af7590847d3309f5f
mrshll0015 <marshall@platarium.com>
2022-12-05 03:53:58 +0200

    +3

--- main.js
+++ main.js
@@ -1,60 +1,46 @@
-const SHA256 = require('crypto-js/sha256');
+'use strict';
+const { Blockchain, Transaction } = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
-class Block{
-	constructor(index, timestamp, data, previousHash = ''){
-		this.index = index;	
-		this.timestamp = timestamp;
-		this.data = data;
-		this.previousHash = previousHash;
-		this.hash = this.calculateHash();			
-	}
+// Your private key goes here
+const myKey = ec.keyFromPrivate(
+  '7c4c45907dec40c91bab3480c39032e90049f1a44f3e18c3e07c23e3273995cf'
+);
 
-	calculateHash(){
-		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
-	}
-}
+// From that we can calculate your public key (which doubles as your wallet address)
+const myWalletAddress = myKey.getPublic('hex');
 
-class Blockchain{
-	constructor(){
-		this.chain = [this.createGenesisBlock()];
-	}
+// Create new instance of Blockchain class
+const PlatariumSmartChain = new Blockchain();
 
-	createGenesisBlock(){
-		return new Block(0, "10/05/1995", "Genesis Block", "0");
-	}
+// Mine first block
+PlatariumSmartChain.minePendingTransactions(myWalletAddress);
 
-	getLatestBlock(){
-		return this.chain[this.chain.length - 1];
-	}
+// Create a transaction & sign it with your key
+const tx1 = new Transaction(myWalletAddress, 'address2', 100);
+tx1.signTransaction(myKey);
+PlatariumSmartChain.addTransaction(tx1);
 
-	addBlock(newBlock){
-		newBlock.previousHash = this.getLatestBlock().hash;
-		newBlock.hash = newBlock.calculateHash();
-		this.chain.push(newBlock);
-	}
+// Mine block
+PlatariumSmartChain.minePendingTransactions(myWalletAddress);
 
-	isChainValid(){
-		for (let i = 1; i < this.chain.length; i++) {
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i - 1];
+// Create second transaction
+const tx2 = new Transaction(myWalletAddress, 'address1', 50);
+tx2.signTransaction(myKey);
+PlatariumSmartChain.addTransaction(tx2);
 
-			if(currentBlock.hash !== currentBlock.calculateHash()){
-				return false;
-			}
+// Mine block
+PlatariumSmartChain.minePendingTransactions(myWalletAddress);
 
-			if(currentBlock.previousHash !== previousBlock.hash){
-				return false;
-			}
-		}
-		return true;
-	}
+console.log();
+console.log(
+  `Balance of mrshl0015 is ${PlatariumSmartChain.getBalanceOfAddress(myWalletAddress)}`
+);
 
-}
+// Uncomment this line if you want to test tampering with the chain
+// PlatariumSmartChain.chain[1].transactions[0].amount = 10;
 
-
-let PlatariumSmartChain = new Blockchain();
-PlatariumSmartChain.addBlock(new Block(1, "15/04/1997", {amount: 8}));
-PlatariumSmartChain.addBlock(new Block(2, "25/04/1997", {amount: 18}));
-
-console.log('is Blockchain valid ' + PlatariumSmartChain.isChainValid());
-// console.log(JSON.stringify(PlatariumSmartChain, null, 3));
+// Check if the chain is valid
+console.log();
+console.log('Blockchain valid:?', PlatariumSmartChain.isChainValid() ? 'Yes' : 'No');
